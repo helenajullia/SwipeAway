94d98baf2517b6fd8e8410713c05fdf331d9f4f4
helenajullia <helenajulliapastore@yahoo.com>
2023-10-25 21:41:32 +0300

    Improved layout for every device

--- lib/authentication/choose_user_or_admin.dart
+++ lib/authentication/choose_user_or_admin.dart
@@ -4,45 +4,49 @@ import 'package:google_fonts/google_fonts.dart';
 class ChooseUserOrAdmin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Color(0xFF171717),
       body: Stack(
         children: [
-          // Title at the top
           Positioned(
             left: 0,
             right: 0,
-            top: 0,
+            top: screenSize.height * 0.00, // Adjust the top position of the "SwipeAway" text
             child: Container(
-              width: 390,
-              height: 120,
+              width: screenSize.width,
+              height: screenSize.height * 0.15,
               decoration: BoxDecoration(color: Color(0xFF454545)),
               child: Stack(
                 alignment: Alignment.center,
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        'SwipeAway',
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.kronaOne(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                          height: 5,
+                      Align(
+                        alignment: Alignment(0.4, 0.6), // Adjust the values (-1.0 to 1.0) to control alignment
+                        child: Text(
+                          'SwipeAway',
+                          style: GoogleFonts.kronaOne(
+                            color: Colors.white,
+
+                            fontSize: screenSize.width * 0.06,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
                       ),
                     ],
                   ),
                   Positioned(
-                    right: 16,
-                    top: 45,
+                    right: screenSize.width * 0.04,
+                    top: screenSize.height * 0.055,
                     child: Image.asset(
-                      'assets/icons/notifications_icon.png', // Replace with the path to your existing icon
-                      width: 30, // Adjust the width as needed
-                      height: 30, // Adjust the height as needed
-                      color: Colors.black, // Adjust the color as needed
+                      'assets/icons/notifications_icon.png',
+                      width: screenSize.width * 0.08,
+                      height: screenSize.width * 0.08,
+                      color: Colors.black,
                     ),
                   ),
                 ],
@@ -50,25 +54,19 @@ class ChooseUserOrAdmin extends StatelessWidget {
             ),
           ),
 
-
-
-
-          // User and Admin Buttons (Centered, Side by Side)
           Positioned(
-            left: 30,
-            top: 250,
+            left: screenSize.width * 0.1,
+            top: screenSize.height * 0.32,
             child: Column(
               children: [
-                // Icon above the buttons
                 Transform.scale(
-                  scale: 3.0, // Increase the scale value to make the icon larger
+                  scale: 2.5,
                   child: Image.asset(
-                    'assets/icons/myAccount_icon.png',
-                    color: Colors.white70,
+                    'assets/icons/user_icon.png',
+                    color: Colors.blueGrey.shade100,
                   ),
                 ),
-                SizedBox(height: 80), // Adjust the space between the icon and buttons
-                // User and Admin Buttons (Centered, Side by Side)
+                SizedBox(height: screenSize.height * 0.1),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -77,46 +75,44 @@ class ChooseUserOrAdmin extends StatelessWidget {
                         // Handle user button click
                       },
                       child: Container(
-                        width: 160,
-                        height: 60,
+                        width: screenSize.width * 0.4,
+                        height: screenSize.height * 0.08,
                         alignment: Alignment.center,
                         decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(30),
+                          borderRadius: BorderRadius.circular(screenSize.width * 0.08),
                         ),
                         child: Text(
                           'User',
                           textAlign: TextAlign.center,
                           style: GoogleFonts.kronaOne(
                             color: Colors.black,
-                            fontSize: 20,
+                            fontSize: screenSize.width * 0.05,
                             fontWeight: FontWeight.w400,
-                            height: 0,
                           ),
                         ),
                       ),
                     ),
-                    SizedBox(width: 16),
+                    SizedBox(width: screenSize.width * 0.04),
                     InkWell(
                       onTap: () {
                         // Handle admin button click
                       },
                       child: Container(
-                        width: 160,
-                        height: 60,
+                        width: screenSize.width * 0.4,
+                        height: screenSize.height * 0.08,
                         alignment: Alignment.center,
                         decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(30),
+                          borderRadius: BorderRadius.circular(screenSize.width * 0.08),
                         ),
                         child: Text(
                           'Admin',
                           textAlign: TextAlign.center,
                           style: GoogleFonts.kronaOne(
                             color: Colors.black,
-                            fontSize: 20,
+                            fontSize: screenSize.width * 0.05,
                             fontWeight: FontWeight.w400,
-                            height: 0,
                           ),
                         ),
                       ),
@@ -126,94 +122,40 @@ class ChooseUserOrAdmin extends StatelessWidget {
               ],
             ),
           ),
-
-
-          // Navigation (Moved to the bottom)
           Positioned(
             left: 0,
             right: 0,
             bottom: 0,
             child: Container(
-              width: 390,
-              height: 100, // You can adjust the height
+              width: screenSize.width,
+              height: screenSize.height * 0.1,
               decoration: BoxDecoration(color: Color(0xFF454545)),
-              child: Column(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  SizedBox(height: 20), // Adjust the height as needed
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      // Wrap each Column with Align to align the content at the bottom
-                      Column(
-                        children: [
-                          Image.asset(
-                            'assets/icons/home_icon.png',
-                            width: 30,
-                            height: 30,
-                            color: Colors.black,
+                  for (final icon in [
+                    'home_icon.png',
+                    'saved_icon.png',
+                    'bookings_icon.png',
+                    'myAccount_icon.png',
+                  ])
+                    Column(
+                      children: [
+                        Image.asset(
+                          'assets/icons/$icon',
+                          width: screenSize.width * 0.08,
+                          height: screenSize.width * 0.15,
+                          color: Colors.black,
+                        ),
+                        Text(
+                          '  ${_getLabelForIcon(icon)}   ',
+                          style: GoogleFonts.kronaOne(
+                            color: Colors.white,
+                            fontSize: screenSize.width * 0.02,
                           ),
-                          Text(
-                            '  Home   ',
-                            style: GoogleFonts.kronaOne(
-                              color: Colors.white,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Image.asset(
-                            'assets/icons/saved_icon.png',
-                            width: 30,
-                            height: 30,
-                            color: Colors.black,
-                          ),
-                          Text(
-                            'Saved   ',
-                            style: GoogleFonts.kronaOne(
-                              color: Colors.white,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Image.asset(
-                            'assets/icons/bookings_icon.png',
-                            width: 30,
-                            height: 30,
-                            color: Colors.black,
-                          ),
-                          Text(
-                            'Bookings   ',
-                            style: GoogleFonts.kronaOne(
-                              color: Colors.white,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Image.asset(
-                            'assets/icons/myAccount_icon.png',
-                            width: 30,
-                            height: 30,
-                            color: Colors.black,
-                          ),
-                          Text(
-                            'MyAccount',
-                            style: GoogleFonts.kronaOne(
-                              color: Colors.white,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                      ],
+                    ),
                 ],
               ),
             ),
@@ -221,5 +163,20 @@ class ChooseUserOrAdmin extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  String _getLabelForIcon(String iconName) {
+    switch (iconName) {
+      case 'home_icon.png':
+        return 'Home';
+      case 'saved_icon.png':
+        return 'Saved';
+      case 'bookings_icon.png':
+        return 'Bookings';
+      case 'myAccount_icon.png':
+        return 'MyAccount';
+      default:
+        return '';
+    }
   }
 }
