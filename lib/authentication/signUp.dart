dcc01b04718b36a01205491bb9f3299348fa7dc3
Nistor Carina <nistorcarina@carinusMacbook.local>
2023-10-27 23:07:03 +0300

    Created the design for the Sign Up page, without the register credentials

---
+++ lib/authentication/signUp.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  int _currentPageIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF454545),
+        leading: IconButton(
+          icon: Image.asset('assets/icons/left_arrow2.png'), // Replace the icon with a left arrow
+          onPressed: () {
+            Navigator.of(context).pop(); // Add navigation action here
+          },
+        ),
+        title: Text(
+          'SwipeAway',
+          style: GoogleFonts.kronaOne(
+            color: Colors.white,
+            fontSize: MediaQuery.of(context).size.width * 0.06,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Image.asset(
+              'assets/icons/notifications_icon.png',
+              color: Colors.black,
+            ),
+            onPressed: () {
+              // Handle notifications icon tap
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: _getPageContent(_currentPageIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xFF454545),
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icons/home_icon.png',
+              width: MediaQuery.of(context).size.width * 0.08,
+              height: MediaQuery.of(context).size.width * 0.15,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icons/saved_icon.png',
+              width: MediaQuery.of(context).size.width * 0.08,
+              height: MediaQuery.of(context).size.width * 0.15,
+            ),
+            label: 'Saved',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icons/bookings_icon.png',
+              width: MediaQuery.of(context).size.width * 0.08,
+              height: MediaQuery.of(context).size.width * 0.15,
+            ),
+            label: 'Bookings',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icons/myAccount_icon.png',
+              width: MediaQuery.of(context).size.width * 0.08,
+              height: MediaQuery.of(context).size.width * 0.15,
+            ),
+            label: 'My Account',
+          ),
+        ],
+        currentIndex: _currentPageIndex,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget _getPageContent(int index) {
+    switch (index) {
+      case 0:
+        return Text('Home Page Content');
+      case 1:
+        return Text('Saved Page Content');
+      case 2:
+        return Text('Bookings Page Content');
+      case 3:
+        return Text('My Account Page Content');
+      default:
+        return Text('Invalid Page');
+    }
+  }
+}
--- lib/authentication/signing_option.dart
+++ lib/authentication/signing_option.dart
@@ -1,181 +1,150 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:swipe_away/authentication/signUp.dart';
 
-class ChooseSigningOption extends StatelessWidget {
+
+class ChooseSigningOption extends StatefulWidget {
+  @override
+  _ChooseSigningOptionState createState() => _ChooseSigningOptionState();
+}
+
+class _ChooseSigningOptionState extends State<ChooseSigningOption> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-
     return Scaffold(
       backgroundColor: Color(0xFF171717),
-      body: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            right: 0,
-            top: screenSize.height * 0.00, // Adjust the top position of the "SwipeAway" text
-            child: Container(
-              width: screenSize.width,
-              height: screenSize.height * 0.15,
-              decoration: BoxDecoration(color: Color(0xFF454545)),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Align(
-                        alignment: Alignment(0.4, 0.6), // Adjust the values (-1.0 to 1.0) to control alignment
-                        child: Text(
-                          'SwipeAway',
-                          style: GoogleFonts.kronaOne(
-                            color: Colors.white,
-                            fontSize: screenSize.width * 0.06,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Positioned(
-                    right: screenSize.width * 0.04,
-                    top: screenSize.height * 0.055,
-                    child: Image.asset(
-                      'assets/icons/notifications_icon.png',
-                      width: screenSize.width * 0.08,
-                      height: screenSize.width * 0.08,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF454545),
+        title: Text(
+          'SwipeAway',
+          style: GoogleFonts.kronaOne(
+            color: Colors.white,
+            fontSize: MediaQuery.of(context).size.width * 0.06,
+            fontWeight: FontWeight.w400,
           ),
-
-          Positioned(
-            left: screenSize.width * 0.1,
-            top: screenSize.height * 0.32,
-            child: Column(
-              children: [
-                Transform.scale(
-                  scale: 2.5,
-                  child: Image.asset(
-                    'assets/icons/user_icon.png',
-                    color: Colors.blueGrey.shade100,
-                  ),
-                ),
-                SizedBox(height: screenSize.height * 0.1),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        // Handle user button click
-                      },
-                      child: Container(
-                        width: screenSize.width * 0.4,
-                        height: screenSize.height * 0.08,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(screenSize.width * 0.08),
-                        ),
-                        child: Text(
-                          'Sign In',
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.kronaOne(
-                            color: Colors.black,
-                            fontSize: screenSize.width * 0.05,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: screenSize.width * 0.04),
-                    InkWell(
-                      onTap: () {
-                        // Handle admin button click
-                      },
-                      child: Container(
-                        width: screenSize.width * 0.4,
-                        height: screenSize.height * 0.08,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(screenSize.width * 0.08),
-                        ),
-                        child: Text(
-                          'Sign Up',
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.kronaOne(
-                            color: Colors.black,
-                            fontSize: screenSize.width * 0.05,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+        ),
+        actions: [
+          IconButton(
+            icon: Image.asset(
+              'assets/icons/notifications_icon.png',
+              color: Colors.black,
             ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              width: screenSize.width,
-              height: screenSize.height * 0.1,
-              decoration: BoxDecoration(color: Color(0xFF454545)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  for (final icon in [
-                    'home_icon.png',
-                    'saved_icon.png',
-                    'bookings_icon.png',
-                    'myAccount_icon.png',
-                  ])
-                    Column(
-                      children: [
-                        Image.asset(
-                          'assets/icons/$icon',
-                          width: screenSize.width * 0.08,
-                          height: screenSize.width * 0.15,
-                          color: Colors.black,
-                        ),
-                        Text(
-                          '  ${_getLabelForIcon(icon)}   ',
-                          style: GoogleFonts.kronaOne(
-                            color: Colors.white,
-                            fontSize: screenSize.width * 0.02,
-                          ),
-                        ),
-                      ],
-                    ),
-                ],
-              ),
-            ),
+            onPressed: () {
+              // Handle notifications icon tap
+            },
           ),
         ],
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Transform.scale(
+              scale: 2.5,
+              child: Image.asset(
+                'assets/icons/user_icon.png',
+                color: Colors.blueGrey.shade100,
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    // Handle sign in button click
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(50, 50),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.8),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign In',
+                    style: GoogleFonts.kronaOne(
+                      color: Colors.black,
+                      fontSize: MediaQuery.of(context).size.width * 0.05,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle sign up button click
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignUp()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(50, 50),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.08),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign Up',
+                    style: GoogleFonts.kronaOne(
+                      color: Colors.black,
+                      fontSize: MediaQuery.of(context).size.width * 0.05,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icons/home_icon.png',
+              width: MediaQuery.of(context).size.width * 0.08,
+              height: MediaQuery.of(context).size.width * 0.15,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icons/saved_icon.png',
+              width: MediaQuery.of(context).size.width * 0.08,
+              height: MediaQuery.of(context).size.width * 0.15,
+            ),
+            label: 'Saved',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icons/bookings_icon.png',
+              width: MediaQuery.of(context).size.width * 0.08,
+              height: MediaQuery.of(context).size.width * 0.15,
+            ),
+            label: 'Bookings',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icons/myAccount_icon.png',
+              width: MediaQuery.of(context).size.width * 0.08,
+              height: MediaQuery.of(context).size.width * 0.15,
+            ),
+            label: 'My Account',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
     );
-  }
-
-  String _getLabelForIcon(String iconName) {
-    switch (iconName) {
-      case 'home_icon.png':
-        return 'Home';
-      case 'saved_icon.png':
-        return 'Saved';
-      case 'bookings_icon.png':
-        return 'Bookings';
-      case 'myAccount_icon.png':
-        return 'MyAccount';
-      default:
-        return '';
-    }
   }
 }
