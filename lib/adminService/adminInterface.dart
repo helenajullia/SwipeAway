eb0bbd35aec1fd7c7182c02d480cb9ea8645ebad
helenajullia <helenajulliapastore@yahoo.com>
2023-11-04 20:24:03 +0200

    Started Design for Admin Dashboard

--- lib/adminService/adminInterface.dart
+++ lib/adminService/adminInterface.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// other imports as necessary
+import 'package:google_fonts/google_fonts.dart';
 
 class AdminInterface extends StatefulWidget {
   @override
@@ -7,40 +7,118 @@ class AdminInterface extends StatefulWidget {
 }
 
 class _AdminInterfaceState extends State<AdminInterface> {
-  // You might want to track admin related data here
+  int _selectedDestination = 0;
+
+  void selectDestination(int index) {
+    setState(() {
+      _selectedDestination = index;
+    });
+    // Here, you could navigate to other pages or perform other actions based on the menu selection
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Admin Dashboard"),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: () {
-              // Implement sign out functionality
-            },
-          ),
-        ],
+        title: Text(
+          "Admin Dashboard",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+         actions: [
+           IconButton(
+            icon: Icon(Icons.settings),
+             onPressed: () {
+               // Navigate to settings page or show sign out option
+             },
+           ),
+         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
           children: <Widget>[
-            // You can add more Widgets here for your admin interface
-            ElevatedButton(
-              onPressed: () {
-                // Add event or action for this button
-              },
-              child: Text('Manage Users'),
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Text(
+                'Admin Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontFamily: 'Roboto',
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                // Add event or action for this button
-              },
-              child: Text('Manage Hotels'),
+            ListTile(
+              title: Text('Dashboard', style: GoogleFonts.roboto()),
+              selected: _selectedDestination == 0,
+              onTap: () => selectDestination(0),
             ),
-            // ... more buttons or features
+            ListTile(
+              title: Text('Manage Users', style: GoogleFonts.roboto()),
+              selected: _selectedDestination == 1,
+              onTap: () => selectDestination(1),
+            ),
+            ListTile(
+              title: Text('Manage Hotels', style: GoogleFonts.roboto()),
+              selected: _selectedDestination == 2,
+              onTap: () => selectDestination(2),
+            ),
+            ListTile(
+              title: Text('Manage Bookings', style: GoogleFonts.roboto()),
+              selected: _selectedDestination == 3,
+              onTap: () => selectDestination(3),
+            ),
+            // Add other ListTile widgets for more menu items...
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // Add Widgets here for your admin interface
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.people, color: Colors.black),
+                  title: Text('Manage Users', style: GoogleFonts.roboto()),
+                  onTap: () {
+                    // Handle manage users
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.hotel, color: Colors.black),
+                  title: Text('Manage Hotels', style: GoogleFonts.roboto()),
+                  onTap: () {
+                    // Handle manage hotels
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.luggage, color: Colors.black),
+                  title: Text('Manage Bookings', style: GoogleFonts.roboto()),
+                  onTap: () {
+                    // Handle manage hotels
+                  },
+                ),
+              ),
+            ),
+            // ... more cards for other options
           ],
         ),
       ),
