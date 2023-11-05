9b44cb1a5864bfaa6541e5af4abb95c55292b505
nistorcarina <carinanistor123321@gmail.com>
2023-11-05 12:30:51 +0200

    Made the change password page

--- lib/home/myAccount/settings/settingsPage.dart
+++ lib/home/myAccount/settings/settingsPage.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:swipe_away/home/myAccount/settings/changePassword.dart';
-import 'package:swipe_away/home/myAccount/settings/notificationSettingsPage.dart';
 import 'package:swipe_away/home/myAccount/settings/themePage.dart';
 import 'package:swipe_away/home/myAccount/settings/walletPage.dart';
 
-//import 'notificationSettingsPage.dart';
+import 'notificationSettingsPage.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
@@ -21,25 +20,27 @@ class SettingsPage extends StatelessWidget {
           tiles: [
             ListTile(
               title: Text('Theme', style: GoogleFonts.roboto()),
-              leading: Icon(Icons.palette,color: Colors.black),
+              leading: Icon(Icons.palette),
               onTap: () {
+                // This will push the ThemePage onto the navigation stack
                 Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => ThemePage(),
+                  builder: (context) => ThemePage(), // Replace ThemePage with your actual theme page widget
                 ));
               },
             ),
             ListTile(
               title: Text('Notification Settings', style: GoogleFonts.roboto()),
-              leading: Icon(Icons.notifications,color: Colors.black),
+              leading: Icon(Icons.notifications),
               onTap: () {
+                // This will push the ThemePage onto the navigation stack
                 Navigator.of(context).push(MaterialPageRoute(
                   builder: (context) => NotificationSettingsPage(), // Replace ThemePage with your actual theme page widget
                 ));
-              }
+              },
             ),
             ListTile(
               title: Text('Wallet', style: GoogleFonts.roboto()),
-              leading: Icon(Icons.account_balance_wallet,color: Colors.black),
+              leading: Icon(Icons.account_balance_wallet),
               onTap: () {
                 Navigator.of(context).push(MaterialPageRoute(
                     builder: (context) => WalletPage(),
@@ -48,7 +49,7 @@ class SettingsPage extends StatelessWidget {
             ),
             ListTile(
               title: Text('Change Password', style: GoogleFonts.roboto()),
-              leading: Icon(Icons.lock,color: Colors.black),
+              leading: Icon(Icons.lock),
               onTap: () {
                 Navigator.of(context).push(MaterialPageRoute(
                     builder: (context) => ChangePassword(),
