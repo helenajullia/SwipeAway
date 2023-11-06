f0dc44918a2e2d072d612855125fffce93d0f50f
nistorcarina <carinanistor123321@gmail.com>
2023-11-06 11:47:42 +0200

    Enabling dark mode on change password page

--- lib/home/myAccount/settings/changePassword.dart
+++ lib/home/myAccount/settings/changePassword.dart
@@ -10,7 +10,6 @@ class ChangePassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.black,
         elevation: 0,
