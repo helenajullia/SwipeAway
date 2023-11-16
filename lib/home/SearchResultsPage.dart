a003ad8966a8561cacf72eb50b8ccedace64ba11
helenajullia <helenajulliapastore@yahoo.com>
2023-11-16 17:06:05 +0200

    still troubleshooting list implementation

--- lib/home/SearchResultsPage.dart
+++ lib/home/SearchResultsPage.dart
@@ -17,35 +17,117 @@ class SearchResultsPage extends StatefulWidget {
 
 class _SearchResultsPageState extends State<SearchResultsPage> {
   final SwiperController swiperController = SwiperController();
+  String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+
+  List<Hotel> hotelsInList = [];
 
   // Method to save the hotel to Firestore
-  void saveHotel(Hotel hotel) async {
-    String userId = FirebaseAuth.instance.currentUser!.uid;
-
-    var existingHotel = await FirebaseFirestore.instance
+  // Method to save the hotel to Firestore within a specific list
+  void saveHotel(Hotel hotel, String listId) async {
+    var hotelCollection = FirebaseFirestore.instance
         .collection('users')
         .doc(userId)
-        .collection('savedHotels')
+        .collection('customLists')
+        .doc(listId)
+        .collection('items');
+
+    var existingHotel = await hotelCollection
         .where('name', isEqualTo: hotel.name)
         .limit(1)
         .get();
 
     if (existingHotel.docs.isEmpty) {
-      await FirebaseFirestore.instance
-          .collection('users')
-          .doc(userId)
-          .collection('savedHotels')
-          .add(hotel.toMap());
-    } else {
-      // Show a message if the hotel already exists
+      await hotelCollection.add(hotel.toMap());
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('You have already saved this hotel.'),
-        ),
+        SnackBar(content: Text('Hotel saved successfully.')),
+      );
+      refreshHotelList(listId); // Refresh the list to show the newly added hotel
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('This hotel is already in the list.')),
       );
     }
   }
 
+
+// Function to fetch hotels from a list
+  Future<List<Hotel>> fetchHotelsFromList(String listId) async {
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('customLists')
+        .doc(listId)
+        .collection('items')
+        .get();
+
+    // Cast each document data to `Map<String, dynamic>` before passing it to `Hotel.fromMap`
+    return snapshot.docs
+        .map((doc) => Hotel.fromMap(doc.data() as Map<String, dynamic>))
+        .toList();
+  }
+
+// Call this function after you save a hotel and want to refresh the list
+  void refreshHotelList(String listId) async {
+    List<Hotel> hotels = await fetchHotelsFromList(listId);
+    // Update your state and UI with the fetched hotels
+    setState(() {
+      // Assuming you have a state variable that holds the list of hotels
+      this.hotelsInList = hotels;
+    });
+  }
+
+  // Method to prompt the user to select or add a list
+
+
+  // Method to create a new list and return its ID
+  Future<String?> _createNewList(BuildContext context) async {
+    TextEditingController listNameController = TextEditingController();
+
+    String? newListId = await showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Create New List'),
+          content: TextField(
+            controller: listNameController,
+            decoration: InputDecoration(hintText: "List Name"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Create'),
+              onPressed: () async {
+                String listName = listNameController.text;
+                if (listName.isNotEmpty) {
+                  DocumentReference newList = await FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(userId)
+                      .collection('customLists')
+                      .add({'name': listName});
+                  Navigator.of(context).pop(newList.id);
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+    return newListId; // This should be the ID of the newly created list
+  }
+
+  // This method is invoked when the user decides to save a hotel.
+  void _onSaveHotel(Hotel hotel) async {
+    String? selectedListId = await _promptSelectList(context);
+    if (selectedListId != null && selectedListId.isNotEmpty) {
+      // Proceed with saving the hotel to the selected list
+      saveHotel(hotel, selectedListId);
+    }
+  }
 
   void _onSwipeUp(int index) {
     showDialog(
@@ -64,9 +146,9 @@ class _SearchResultsPageState extends State<SearchResultsPage> {
           TextButton(
             child: Text("Save"),
             onPressed: () {
-              saveHotel(widget.searchResults[index]);
-              Navigator.of(context).pop(); // Dismiss the dialog
-              swiperController.next(); // Move to the next hotel
+              // Here you should pass the listId as well. Assuming you have it available.
+              _onSaveHotel(widget.searchResults[index]);
+              Navigator.of(context).pop();
             },
           ),
         ],
@@ -109,5 +191,69 @@ class _SearchResultsPageState extends State<SearchResultsPage> {
       )
           : Center(child: Text('No hotels found')),
     );
+  }
+
+  Future<String?> _promptSelectList(BuildContext context) async {
+    String? selectedListId;
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+
+    // Fetch the list names from Firestore
+    QuerySnapshot listSnapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('customLists')
+        .get();
+
+    List<Map<String, String>> listNames = listSnapshot.docs
+        .map((doc) {
+      var data = doc.data() as Map<String, dynamic>?; // The data can be null, hence the '?'
+      // Use null-aware operators to handle possible null values
+      var name = data?['name'];
+      if (name is String) {
+        return {
+          'id': doc.id, // This is the document ID
+          'name': name,
+        };
+      }
+      return null; // Return null if the document's data is null or 'name' is not a string
+    })
+        .where((item) => item != null) // Remove nulls
+        .cast<Map<String, String>>() // Cast the non-null items to the correct type
+        .toList();
+
+
+    // Show the dialog
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Select a List'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: listNames.map((item) {
+                return ListTile(
+                  title: Text(item['name'] as String),
+                  onTap: () {
+                    selectedListId = item['id']; // This should be the actual document ID
+                    Navigator.of(context).pop();
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Create New List'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog before opening a new one
+                _createNewList(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    return selectedListId; // Return the selected list ID
   }
 }
